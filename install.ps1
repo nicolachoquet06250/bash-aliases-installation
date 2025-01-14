0d87b74f81828cd29596633e89aafc28fffeb7fb
nchoquet <nicolachoquet06250@gmail.com>
2025-01-14 16:45:25 +0100

    Fix sur framework, clipboard et norsys

--- install.ps1
+++ install.ps1
@@ -5,13 +5,15 @@ $github_repo_search_url_base="https://api.github.com/search/repositories?q=nicol
 for ($i=0; $i -lt $repos.Length; $i++) {
     $repo_name = $repos[$i]
 
-    Write-Output $repo_name
+    Write-Output "============ ${repo_name} ============"
 
     $url = "$github_repo_search_url_base/$repo_name"
-    Write-Output $url
-    If ("$env:GITHUB_INSTALL_ACCESS_TOKEN" -ne "") {
+    If ("$env:GITHUB_INSTALL_ACCESS_TOKEN" -ne "")
+    {
         $curl_result = curl -s "$url" --header "Authorization: Bearer $env:GITHUB_INSTALL_ACCESS_TOKEN" | Out-String
-    } else {
+    }
+    else
+    {
         $curl_result = curl -s "$url" | Out-String
     }
 
@@ -19,27 +21,42 @@ for ($i=0; $i -lt $repos.Length; $i++) {
 
     $total_count = $parsedJson.total_count
 
-    If ($total_count -eq 0) {
+    If ($total_count -eq 0)
+    {
         Write-Output "existe pas"
-    } elseif ($total_count -gt 1) {
+    }
+    elseif ($total_count -gt 1)
+    {
         Write-Output "trop grand"
-    } elseif ($total_count -eq 1) {
-        $repo = $parsedJson.items[0].ssh_url
+    }
+    elseif ($total_count -eq 1)
+    {
+        $repo = $parsedJson.items[0].clone_url
 
-        If (-not (Test-Path "$(Get-Location)/${repo_name}")) {
+        If (-not (Test-Path "$( Get-Location )/${repo_name}"))
+        {
             git clone $repo
         }
 
-        If ((Test-Path "$(Get-Location)/${repo_name}/.githooks/post-checkout.ps1")) {
-            If ((Test-Path "$(Get-Location)/${repo_name}/.git/hooks/post-checkout.ps1")) {
-                Remove-Item -Recurse -Force "$(Get-Location)/${repo_name}/.git/hooks/post-checkout.ps1"
+        If ((Test-Path "$( Get-Location )/${repo_name}/.githooks/post-checkout.ps1"))
+        {
+            If ((Test-Path "$( Get-Location )/${repo_name}/.git/hooks/post-checkout.ps1"))
+            {
+                Remove-Item -Recurse -Force "$( Get-Location )/${repo_name}/.git/hooks/post-checkout.ps1"
             }
-            New-Item -ItemType SymbolicLink -Path "$(Get-Location)/${repo_name}/.githooks/post-checkout.ps1" -Target "$(Get-Location)/${repo_name}/.git/hooks/post-checkout.ps1"
+            If (-not (Test-Path "$( Get-Location )/${repo_name}/.git/hooks/post-checkout.ps1"))
+            {
+                New-Item -ItemType SymbolicLink -Path "$( Get-Location )/${repo_name}/.git/hooks/post-checkout.ps1" -Target "$( Get-Location )/${repo_name}/.githooks/post-checkout.ps1"
 
-            Set-ExecutionPolicy Bypass -command "$(Get-Location)/${repo_name}/.githooks/post-checkout.ps1"
-
-            . "$(Get-Location)/${repo_name}/.git/hooks/post-checkout.ps1" "install-script"
-        } else {
+                powershell "$( Get-Location )/${repo_name}/.githooks/post-checkout.ps1" "install-script"
+            }
+            else
+            {
+                Write-Output "Repository ${repo_name} already installed"
+            }
+        }
+        else
+        {
             Write-Output "Pas de version Powershell"
         }
     }
